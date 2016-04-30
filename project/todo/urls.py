009c94eb61092cbd08c9e937836ae651d82b1ea2
dagothar <dagothar@gmail.com>
2016-04-30 17:12:09 +0200

    improved templates

--- project/todo/urls.py
+++ project/todo/urls.py
@@ -4,6 +4,22 @@ import views
 
 
 urlpatterns = [
-    # single day view, e.g.: todo/16/04/30
-    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})', views.DayView.as_view(), name='day_view'),
+    # today view, e.g.: todo/today
+    url(
+        r'^today',
+        views.TodayView.as_view(),
+        name='today_view'
+    ),
+    # single day view, e.g.: todo/2016/04/30
+    url(
+        r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})',
+        views.DayView.as_view(),
+        name='day_view'
+    ),
+    # month view, e.g.: todo/2016/04
+    url(
+        r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})',
+        views.MonthView.as_view(),
+        name='month_view'
+    ),
 ]
--- project/todo/views/__init__.py
+++ project/todo/views/__init__.py
@@ -1,1 +1,3 @@
+from today_view import TodayView
 from day_view import DayView
+from month_view import MonthView
