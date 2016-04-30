d6e5a9864269d0caf614872efd20c923c885c567
dagothar <dagothar@gmail.com>
2016-04-30 16:40:25 +0200

    require login for tasks view

--- project/todo/views/day_view.py
+++ project/todo/views/day_view.py
@@ -1,11 +1,12 @@
 from django.views.generic import ListView
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 import datetime
 
 from .. import models
 
 
-class DayView(ListView):
+class DayView(LoginRequiredMixin, ListView):
     """
     Single day view.
     """
@@ -13,10 +14,24 @@ class DayView(ListView):
     template_name = 'todo/day_view.html'
     context_object_name = 'tasks'
 
-    def get_queryset(self):
-        date = datetime.date(
+    def get_date(self):
+        """
+        Constructs date from the view url parameters.
+
+        Returns:
+            date: Date object constructed from the url path, e.g.:
+                todo/2016/04/30 yields 2016-04-30 date.
+        """
+        return datetime.date(
             int(self.kwargs['year']),
             int(self.kwargs['month']),
             int(self.kwargs['day'])
         )
-        return models.Task.objects.filter(date__exact=date)
+
+    def get_queryset(self):
+        """Returns a list of tasks of the logged in user for a given day."""
+        date = self.get_date()
+        return models.Task.objects.filter(
+            user__exact=self.request.user,
+            date__exact=date
+        )
