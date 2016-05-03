3407203f84c3ffc1a54e29713aacb36d8ba17763
dagothar <dagothar@gmail.com>
2016-05-03 13:31:35 +0200

    small changes

--- project/todo/views/month_view.py
+++ project/todo/views/month_view.py
@@ -1,5 +1,28 @@
 from django.views.generic import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+from calendar import HTMLCalendar
+
+
+class TaskCalendar(HTMLCalendar):
+    def formatday(self, day, weekday):
+        return super.formatday(day, weekday)
+    pass
 
 
 class MonthView(View):
-    pass
+    """Presents a month view of tasks."""
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        """Processes the GET request."""
+        year = int(kwargs['year'])
+        month = int(kwargs['month'])
+
+        calendar = HTMLCalendar()
+        ctx = {
+            'calendar': mark_safe(calendar.formatmonth(year, month))
+        }
+        return render(request, 'todo/month_view.html', ctx)
